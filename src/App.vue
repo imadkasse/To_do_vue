f29e16fc8bf5b0051f9abaab90bc3109c957590a
imad kasse <kasseimad81@gmail.com>
2025-02-08 22:15:20 +0100

    First_vue_app

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<script setup>
+import Todo from "./components/Todo.vue";
+import { ref } from "vue";
+// const tasks = localStorage.getItem("tasks")
+//   ? JSON.parse(localStorage.getItem("tasks"))
+//   : ref([]);
+const tasks = ref(
+  localStorage.getItem("tasks")
+    ? JSON.parse(localStorage.getItem("tasks"))
+    : ref([])
+);
+const taskName = ref("");
+const addTask = () => {
+  if (taskName) {
+    const newTask = {
+      id: Math.random(),
+      name: taskName.value,
+      completed: false,
+    };
+    tasks.value.push(newTask);
+    taskName.value = "";
+    localStorage.setItem("tasks", JSON.stringify(tasks.value));
+  }
+};
+const deleteTask = (id) => {
+  tasks.value = tasks.value.filter((task) => task.id !== id);
+  localStorage.setItem("tasks", JSON.stringify(tasks.value));
+};
+
+const completedMark = (id) => {
+  tasks.value = tasks.value.map((task) => {
+    if (task.id === id) {
+      task.completed = true;
+      localStorage.setItem("tasks", JSON.stringify(tasks.value));
+      location.reload();
+    }
+  });
+};
+</script>
+
+<template>
+  <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-lg">
+    <h1 className="text-2xl font-bold mb-4 text-gray-800">Todo List</h1>
+    <div className="flex mb-4">
+      <input
+        type="text"
+        v-model="taskName"
+        placeholder="Add a new todo"
+        class="flex-grow mr-2 pl-2 border-2 border-blue-400 rounded-md"
+      />
+      <button
+        @click="addTask"
+        class="font-semibold text-white py-2 px-3 rounded-md bg-blue-400 hover:bg-blue-500 transition duration-300"
+      >
+        إضافة
+      </button>
+    </div>
+    <div class="flex flex-col gap-2" v-for="(task, index) in tasks">
+      <Todo
+        :id="task.id"
+        :deleteTask="deleteTask"
+        :completedMark="completedMark"
+        :key="index"
+        :taskName="task.name"
+        :completed="task.completed"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex items-center bg-gray-100 p-3 rounded my-2">
+    <!-- ربط خاصية checked بالقيمة من props -->
+    <input
+      type="checkbox"
+      :checked="completed"
+      @click="handleCompleted"
+      class="mr-2"
+    />
+    <span :class="{ 'line-through': completed }" class="flex-grow">{{
+      taskName
+    }}</span>
+    <button
+      @click="handleDelete"
+      class="font-semibold text-red-400 hover:text-red-500 transition duration-300"
+    >
+      حذف
+    </button>
+  </div>
+</template>
+
+<script setup>
+// تعريف الخصائص (props) المطلوبة من المكون الأب
+const props = defineProps({
+  id: Number,
+  taskName: String,
+  completed: Boolean,
+  deleteTask: Function,
+  completedMark: Function,
+});
+
+// دالة الحذف
+const handleDelete = () => {
+  props.deleteTask(props.id); // تمرير اسم المهمة عند الحذف
+};
+const handleCompleted = () => {
+  props.completedMark(props.id);
+};
+</script>
+
+<style scoped></style>
